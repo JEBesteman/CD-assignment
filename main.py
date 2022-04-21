f9ce2ca93dd9699337e6ce786707bba0ce66b74f
Jolanda Besteman <jebesteman@gmail.com>
2022-04-21 14:14:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Import what we need from flask
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route("/")
+def index():
+    return "Almost finished!!"
+
+
+@app.route("/cow")
+def cow():
+    return "MOoooOo!"
+
+
+@app.route("/dog")
+def dog():
+    return "Watch out big angry dog!"
---
+++ test_main.py
@@ -0,0 +1,30 @@
+import pytest
+
+from main import app
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_index(client):
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.data == b"Almost finished!!"
+
+
+def test_cow(client):
+    response = client.get("/cow")
+
+    assert response.status_code == 200
+    assert response.data == b"MOoooOo!"
+
+
+def test_dog(client):
+    response = client.get("/dog")
+
+    assert response.status_code == 200
+    assert response.data == b"Watch out big angry dog!"
